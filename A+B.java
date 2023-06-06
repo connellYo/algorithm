e0a94fe285f7210b52f09195a12caaa99932e2c6
Easyliving <118458340+connellYo@users.noreply.github.com>
2023-06-06 22:13:27 +0900

    Create A+B.java

---
+++ A+B.java
@@ -0,0 +1,16 @@
+package Algorithm;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("a를 입력해주세요");
+		a=sc.nextInt();
+		System.out.println("b를 입력해주세요");
+		b=sc.nextInt();
+		System.out.println(a+b);
+
+	}
+}
