bcf231f115495a7f125cc801c65766511dfa0969
Easyliving <118458340+connellYo@users.noreply.github.com>
2023-06-07 22:01:07 +0900

    Create ScannerString.java

---
+++ ScannerString.java
@@ -0,0 +1,15 @@
+package Algorithm;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		String id;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("id를 입력해주세요");
+		id=sc.next();
+		System.out.println(id+"??!");
+
+
+	}
+}
